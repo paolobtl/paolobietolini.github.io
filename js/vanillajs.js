2436b55e65d66843ffa3c5bbfbe7e2b94cf77f73
paolobtl <bietolinipaolo@gmail.com>
2023-05-06 22:37:21 +0200

    added blog

---
+++ js/vanillajs.js
@@ -0,0 +1,13 @@
+// Footer year 
+document.getElementById('currentYear').innerHTML = new Date().getFullYear();
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+    // Hide blog section
+    if (document.querySelector("#blog").innerText == '') {
+        document.querySelector("#blog").remove();
+        document.querySelector("#top > header > nav > ul > li:nth-child(2) > a").remove()
+    };
+  });
